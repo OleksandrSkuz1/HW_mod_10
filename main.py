fe483b3cb2086b78c1d0a45f22aa3b6354645f3b
Oleksandr_Skuz <138138803+OleksandrSkuz1@users.noreply.github.com>
2023-10-31 14:51:09 +0200

    add

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from collections import UserDict
 
 class Field:
-    def __init__(self, value):
+    def __init__(self, value) -> None:
         self.value = value
 
     def __str__(self):
@@ -11,7 +11,7 @@ class Name(Field):
     ...
 
 class Phone(Field):
-    def __init__(self, value):
+    def __init__(self, value) -> None:
         if not self.is_valid_phone(value):
             raise ValueError("Invalid phone number format")
         super().__init__(value)
@@ -21,20 +21,20 @@ class Phone(Field):
         return len(value) == 10 and value.isdigit()
 
 class Record:
-    def __init__(self, name):
+    def __init__(self, name: str) -> None:
         self.name = Name(name)
         self.phones = []
 
-    def add_phone(self, phone):
+    def add_phone(self, phone: int):
         phone = Phone(phone)
         self.phones.append(phone)
 
-    def remove_phone(self, phone):
+    def remove_phone(self, phone: int):
         for p in self.phones:
             if p.value == phone:
                 self.phones.remove(p)
 
-    def edit_phone(self, old_phone, new_phone):
+    def edit_phone(self, old_phone: int, new_phone: int):
         found = False  # Додана змінна для відстеження, чи знайдено номер
         if Phone.is_valid_phone(new_phone):
             for p in self.phones:
