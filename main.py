b9e38afced03eb0ceebd1447eea5a3d6eb6a1fa8
Oleksandr_Skuz <138138803+OleksandrSkuz1@users.noreply.github.com>
2023-10-31 14:46:16 +0200

    add

--- main.py
+++ main.py
@@ -1,28 +1,93 @@
 from collections import UserDict
 
-# Створюємо батьківський клас у ньому реалізується логіка загальна для всіх полів
 class Field:
-    def __init__(self, value) -> None:
+    def __init__(self, value):
         self.value = value
 
     def __str__(self):
         return str(self.value)
 
 class Name(Field):
-    # реалізація класу
+    ...
 
 class Phone(Field):
-    # реалізація класу
+    def __init__(self, value):
+        if not self.is_valid_phone(value):
+            raise ValueError("Invalid phone number format")
+        super().__init__(value)
+
+    @staticmethod
+    def is_valid_phone(value):
+        return len(value) == 10 and value.isdigit()
 
 class Record:
-    def __init__(self, name: str) -> None:
+    def __init__(self, name):
         self.name = Name(name)
         self.phones = []
 
-    # реалізація класу
+    def add_phone(self, phone):
+        phone = Phone(phone)
+        self.phones.append(phone)
+
+    def remove_phone(self, phone):
+        for p in self.phones:
+            if p.value == phone:
+                self.phones.remove(p)
+
+    def edit_phone(self, old_phone, new_phone):
+        found = False  # Додана змінна для відстеження, чи знайдено номер
+        if Phone.is_valid_phone(new_phone):
+            for p in self.phones:
+                if p.value == old_phone:
+                    p.value = new_phone
+                    found = True  # Позначити, що номер знайдено
+            if not found:
+                raise ValueError(f"Phone number '{old_phone}' not found")
+        else:
+            raise ValueError("Invalid phone number format")
+
+
+    def find_phone(self, phone):
+        for p in self.phones:
+            if p.value == phone:
+                return p  
+
 
     def __str__(self):
-        return f"Contact name: {self.name.value}, phones: {'; '.join(p.value for p in self.phones)}"
+        phones_str = "; ".join(str(p) for p in self.phones)
+        return f"Contact name: {self.name.value}, phones: {phones_str}"
 
 class AddressBook(UserDict):
-    # реалізація класу
+    def add_record(self, record):
+        self.data[record.name.value] = record
+
+    def find(self, name):
+        if name in self.data:
+            return self.data[name]
+
+    def delete(self, name):
+        if name in self.data:
+            del self.data[name]
+
+# Приклад використання:
+book = AddressBook()
+john_record = Record("John")
+john_record.add_phone("1234567890")
+john_record.add_phone("5555555555")
+book.add_record(john_record)
+
+jane_record = Record("Jane")
+jane_record.add_phone("9876543210")
+book.add_record(jane_record)
+
+for name, record in book.data.items():
+    print(record)
+
+john = book.find("John")
+john.edit_phone("1234567890", "1112223333")
+print(john)
+
+found_phone = john.find_phone("5555555555")
+print(f"{john.name.value}: {found_phone}")
+
+book.delete("Jane")
